2fd6a139051e3e13010448d7230dd41362e8bea5
Rai-Aaditya <adityaanchalarai@gmail.com>
2023-04-04 23:40:35 +0530

    first commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 const menu = document.getElementById('menu');
 const hamburger = document.getElementById('hamburger');
+const body = document.getElementById('body');
 function menuDisplay(){
     menu.classList.toggle('hidden');
     hamburger.classList.toggle('bg-white');
+}
+
+function changeTheme(){
+    body.classList.toggle('dark');
 }
